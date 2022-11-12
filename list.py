e33f636060e2064a01e41ffe845b8690f36134d3
9534495797 <chaubeyankit927@gmail.com>
2022-11-12 18:58:28 +0530

    okk

---
+++ list.py
@@ -0,0 +1,90 @@
+#x=["red","blue","red","black"]
+#x[0:2] = "green","black"
+#print(x)
+
+
+#y=["red","blue","black"]
+#y.insert(0,"ankit")
+#print(y)
+
+
+x=["red","blue","black"]
+y=["apache","pulser","bmw"]
+z=("virat","dhoni","mahi")
+#x.extend(z)
+#print(x)
+#x=["RAM","SHYAM","HANUMAN"]
+#del x[0]
+#x.pop(1)
+#x.remove("RAM")
+#  print(x)
+
+
+#for i in y:
+#    print(i)
+
+#i=0
+#while i< len(x):
+ #   print(x(i))
+  #  i+=1
+#colors=["red","blue","red","black"]
+#cars=["tata","nano","alto","suzuki"]
+#newlist=[]
+#[print(x) for x in cars]
+
+#for i in cars:
+
+
+  #  if "a" in i:
+   #     newlist.append(i)
+#print(newlist)
+
+
+
+#newlist = [x for x in cars if x != "tata"]
+#print(newlist)
+# expression item list condition
+
+
+#new_list = [x.upper() for x in cars]
+#print(new_list)
+#x=[1,3,5,4,2,8]
+#x.sort()
+#print(x)
+
+#def square(list):
+   # return[i**2 for i in list]
+
+#list=[1,2,3,4,5,]
+#print(square(list))
+#square(list)
+
+
+
+#numbers=[1,2,3,4,5]
+#numbers.reverse()
+#print(numbers)
+
+
+
+
+#numbers=[1,2,3,4,5]
+#num2=numbers.copy()
+#print(num2)
+
+list1= ["x","y","z"]
+list2=[1,2,3]
+#list3=list1+list2
+#print(list3)
+
+#for x in list2:
+ #list1.append(x)
+# print(list1)
+
+list1.extend(list2)
+print(list1)
+
+
+
+
+
